83e8913337161f04bddb8bf981e43b1ef41e2a37
Nikita Vaulin <vaulin@ro.ru>
2022-10-22 04:32:54 +0300

    Added Chessboard array creating

--- HW_4_Numpy/numpy_challenge.py
+++ HW_4_Numpy/numpy_challenge.py
@@ -1,6 +1,22 @@
 import numpy as np
 
 if __name__ == "__main__":
+    # First array
     array_one = np.ones(10)
 
+    # Second array
 
+    # Third array - 8x8 chessboard
+    chessboard_1 = np.zeros((8, 8), dtype=int)
+    chessboard_1[1::2, ::2], chessboard_1[::2, 1::2] = 1, 1
+
+    chessboard_2 = np.ones(35, dtype=int)
+    chessboard_2[::2] = 0
+    chessboard_2 = np.delete(chessboard_2, [8, 17, 26])
+    chessboard_2 = np.stack((chessboard_2, chessboard_2))
+    chessboard_2 = chessboard_2.reshape((8, 8))
+
+    chessboard_3 = np.tile(np.array([[0, 1], [1, 0]]), (4, 4))
+
+    print((chessboard_1 == chessboard_2).all() and (chessboard_2 == chessboard_3).all())
+    print(chessboard_1)  # Checkig all 3 is equal and printing only one
