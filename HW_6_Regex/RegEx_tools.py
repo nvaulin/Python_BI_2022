f99c1162c3338a3cbf0db6fe8f8108351e9c190a
Nikita Vaulin <vaulin@ro.ru>
2022-11-14 13:42:34 +0300

    Format docstrings

--- HW_6_Regex/RegEx_tools.py
+++ HW_6_Regex/RegEx_tools.py
@@ -8,14 +8,14 @@ def file_scanner(filename: str, pattern: str, returning: bool = True,
     Find any patterns in a particular file. Can return them or write into new a file.
 
         Parameters:
-            filename (str): name of a file to search in (can be path as well)
-            pattern (str): pattern to search, ftp-link pattern by default
-            returning (bool): whether to return the list of found matches, default = True
-            writing (bool): whether to write to a file the list of found matches, default = False
-            outname (str): name of a file to write in (can be path as well)
+            *filename* (str): name of a file to search in (can be path as well)\n
+            *pattern* (str): pattern to search, ftp-link pattern by default\n
+            *returning* (bool): whether to return the list of found matches, default = True\n
+            *writing* (bool): whether to write to a file the list of found matches, default = False\n
+            *outname* (str): name of a file to write in (can be path as well)
 
         Returns:
-            (set[str]): set of found patterns
+            *set[str]*: set of found patterns
     """
     pattern = re.compile(pattern)
     with open(filename, 'r') as file:
@@ -39,15 +39,15 @@ def ftp_finder(filename: str,
     Also function can be set to return the list of links and not write them to the file as well.
 
         Parameters:
-            filename (str): name of a file to search in (can be path as well)
+            *filename (str): name of a file to search in (can be path as well)
 
         Optional parameters:
-            returning (bool): whether to return the list of found matches
-            writing (bool): whether to write to a file the list of found matches
-            outname (str): name of a file to write in (can be path as well)
+            *returning* (bool): whether to return the list of found matches\n
+            *writing* (bool): whether to write to a file the list of found matches\n
+            *outname* (str): name of a file to write in (can be path as well)
 
         Returns:
-            (list[str]): list of found ftp-links
+            *list[str]*: list of found ftp-links
     """
     return file_scanner(filename, r"\bftp[\w\d\\/\._#]+?\s", returning, writing, outname)
 
@@ -56,23 +56,32 @@ def numbers_extractor(filename: str, is_float: bool = True, is_exponential: bool
                       decimal_sep: str = '\.', radix_sep: str = '', print_as_is: bool = False) -> set:
     """
     Find any decimal numbers in file. Non-decimal numbers can be extracted as well but considered as decimal numbers.
+
     Decimal and radix separator can be set manually with respect to regex notation, but for common separators there
     provided a translation of a human-format string into a regex-format. If many, please, provide separators as regex
     collection [].
 
+    If both separators specified as '\.', only the last '.' considered as a decimal separator
+
+    *Examples*:
+
+    Radix separator is needed to extract as a single number such numbers as: 10 000 000 or 10.000.000.
+
+    is_exponential flag is needed to extract numbers in scientific notation suc as: 6.26e6
+
         Parameters:
-            filename (str): name of a file to search in (can be path as well)
+            *filename (str): name of a file to search in (can be path as well)
 
         Optional parameters:
-            is_float (bool): whether to consider last "." as a decimal separator
-            is_exponential (bool): whether to consider "e" or "E" as a power separator in scientific notation
-            float_sep (str): decimal separator of float numbers, can be regex collection []
-            radix_sep (str): separator of decimal number radixes (bases), can be regex collection []
-            print_as_is (bool): whether to print found numbers as strings as they are, or to translate into
+            *is_float* (bool): whether to consider last "." as a decimal separator\n
+            *is_exponential* (bool): whether to consider "e" or "E" as a power separator in scientific notation\n
+            *float_sep* (str): decimal separator of float numbers, can be regex collection []\n
+            *radix_sep* (str): separator of decimal number radixes (bases), can be regex collection []\n
+            *print_as_is* (bool): whether to print found numbers as strings as they are, or to translate into
                                 numerical data (with respect to is_float option)
 
         Returns:
-            (set[num]): set of found numbers (set of float/integer numbers or strings if specified)
+            *set[num]*: set of found numbers (set of float/integer numbers or strings if specified)
     """
     separators = {' ': r'\s',
                   '.': r'\.'
@@ -102,7 +111,10 @@ def numbers_extractor(filename: str, is_float: bool = True, is_exponential: bool
     return numbers_found
 
 
+def words_extractor(filename: str, is_float: bool = True, is_exponential: bool = True,
+                    decimal_sep: str = '\.', radix_sep: str = '', print_as_is: bool = False) -> set:
+
+
 if __name__ == '__main__':
     ftp_finder('data/1_references')
     print(numbers_extractor('data/2_2430AD', print_as_is=True))
-
