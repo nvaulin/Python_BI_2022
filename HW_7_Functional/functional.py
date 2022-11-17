a461134f0045a20e448cf70ed3f66524f9f34a27
Nikita Vaulin <vaulin@ro.ru>
2022-11-17 09:56:33 +0300

    Add func_chain tasks

--- HW_7_Functional/functional.py
+++ HW_7_Functional/functional.py
@@ -1,32 +1,58 @@
 import sys
+import numpy as np
 
 
+# Дополнительная функция для заданий 1 и 2 т.к. их две функции были слишком похожи чтобы не объединить в одну
+def sequential_apply(method, *args):
+    args = [*args][::-1]
+    container = args.pop(0)
+    while args:
+        container = list(method(args.pop(), container))
+    return container
+
+
+# Задание 1
 def sequential_map(*args):
-    args = [*args][::-1]
-    container = args.pop(0)
-    while args:
-        container = list(map(args.pop(), container))
-    return container
+    return sequential_apply(map, *args)
 
 
+# Задание 2
 def consensus_filter(*args):
-    args = [*args][::-1]
-    container = args.pop(0)
-    while args:
-        container = list(filter(args.pop(), container))
-    return container
+    return sequential_apply(filter, *args)
 
 
+# Задание 3
 def conditional_reduce(func_1, func_2, container):
-    container = filter(func_1, container)
-    result = sequential_map(func_2)
-    while container:
-        result +=
-    return result
+    container = list(filter(func_1, container))[::-1]
+    while len(container) > 1:
+        container.append(func_2(container.pop(), container.pop()))
+    return container.pop()
 
 
-def func_chain():
-    pass
+# Задаение 4
+def func_chain(*funcs):
+    funcs = [*funcs][::-1]
+
+    def pipe(*args):
+        arguments = list(args)
+        while funcs:
+            arguments = list(map(funcs.pop(), arguments))
+        # map спрятан в наружний список, если там 1 значение решил достать результат из него по-функциональному
+        result = (lambda x: x)(*arguments) if len(arguments) == 1 else arguments
+        return result
+
+    return pipe
+
+
+
+# Интеграция func_chain в 1 задание
+def sequential_map_2(*args):
+    args = list(args)
+    container = args.pop()
+    pipe = func_chain(*args)
+    return pipe(*container)
+
+
 
 
 def multiple_partial():
@@ -35,5 +61,3 @@ def multiple_partial():
 
 def my_print():
     pass
-
-
