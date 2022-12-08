e5dc1837a5e9cd1b3b64a58f72975c273ab58527
Nikita Vaulin <vaulin@ro.ru>
2022-12-08 21:01:29 +0300

    Add ls

---
+++ HW_8_UNIX_tools/ls.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+
+
+def parse_ls_args():
+    parser = argparse.ArgumentParser(
+        usage='ls.py [OPTION] [FILE]',
+        description='''List information about the FILEs (the current directory by default). Sort entries alphabetically''')
+
+    parser.add_argument('-a', '--all', action='store_true', help='do not ignore entries starting with .',
+                        required=False)
+    parser.add_argument('dir', type=str, default='.', nargs='?',
+                        help='directory which content to display ("." by default)')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_ls_args()
+    directory = args.dir
+
+    try:
+        dir_content = list(sorted(os.listdir(directory)))
+        if args.all:
+            dir_content.extend(['.', '..'])
+        else:
+            dir_content = list(filter(lambda x: not x.startswith('.'), dir_content))
+        sys.stdout.write('  '.join(dir_content) + '\n')
+    except FileNotFoundError:
+        sys.stderr.write(f'ls: cannot access \'{directory}\': No such file or directory\n')
